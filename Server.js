1f1a4d111afc3753b61dd24a030c2e2f234f7fbd
Sam99-44 <nsmakhado@gmail.com>
2026-05-24 01:30:01 +0200

    Update Server.js

--- Server.js
+++ Server.js
@@ -362,13 +362,13 @@ const uploadProfilePhoto = multer({
 });
 
 /* ------------------ RATE LIMIT ------------------ */
-const registerLimiter = rateLimit({
+const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 5,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
-    message: "Too many registration attempts. Please try again later.",
+    message: "Too many login attempts. Please try again later.",
   },
 });
 
